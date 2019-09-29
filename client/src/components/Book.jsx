ef5073b46738800def2381ae1bfd7052363a5a7c
Wendy Wu <Wendy@Wendys-MacBook-Air.local>
2019-09-29 16:29:22 -0700

    Add read more feature to every description to expand the text block

--- client/src/components/Book.jsx
+++ client/src/components/Book.jsx
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 const Title = styled.div`
   color: #2b90d9;
   line-height: 1.4;
+  font-size: 16px;
   font-family: 'Open Sans', sans-serif;
 `;
 
@@ -15,8 +16,6 @@ const Author = styled.div`
 
 const Description = styled.div`
   color: #282c37;
-  font-size: 13px;
-  font-family: 'Open Sans', sans-serif;
 `;
 
 const Image = styled.img`
@@ -31,21 +30,59 @@ const Image = styled.img`
 
 const Wrapper = styled.section`
   padding: 2em;
+  max-width: 500px;
+  font-family: 'Open Sans', sans-serif;
+  font-size: 13px;
 `;
 
+const ReadMore = styled.span`
+  color: #2b90d9;
+  font-size: 13px;
+`;
 
-const Book = (props) => {
-  return (
+class Book extends React.Component{
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      expanded: false,
+    }
+    this.expandText = this.expandText.bind(this);
+  }
+  expandText() {
+    this.setState({
+      expanded: true,
+    });
+  }
+  render() {
+    let description = <Description>{this.props.description}</Description>;
+    if (this.props.description.length > 100) {
+      description = (
+        <span>
+          <span>
+            {this.props.description.slice(0, 200)} ...
+          </span>
+            <ReadMore onClick={this.expandText}>Read more</ReadMore>
+        </span>
+      );
+    }
+    if (this.state.expanded) {
+      description = <Description>{this.props.description}</Description>
+    }
+
+    return (
     <div>
-      <Image src={`${props.image}`} />
+      <Image src={`${this.props.image}`} />
       <Wrapper>
-      <Title>{props.title}</Title>
-      <Author>By {props.author}</Author>
+      <Title>{this.props.title}</Title>
+      <Author>By {this.props.author}</Author>
       <br/>
-      <Description>{props.description}</Description>
+      {description}
       </Wrapper>
     </div>
-  );
-};
+    );
+  }
+}
+
 
 export default Book;
