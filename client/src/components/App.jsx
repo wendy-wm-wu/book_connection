f0688e1787dd9245a5ae13cf88c7014ed8d1fdfe
wendy-wm-wu <wendy.wm.wu@gmail.com>
2019-11-11 18:54:02 -0800

    Add Nav bar component to App

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import NavBar from './NavBar.jsx';
 import User from './user.jsx';
 import Home from './Home.jsx';
 import Login from './Login.jsx';
@@ -9,10 +10,15 @@ import history from './history';
 export default function App() {
   return (
     <Router history = {history}>
-      <Route exact path = '/' component = { Home } />
-      <Route path = '/login' component = { Login } />
-      <Route path = '/signup' component = { Signup } />
-      <Route path = '/user' component = { User } />
+      <div>
+        <NavBar />
+        <Switch>
+          <Route exact path = '/' component = { Home } />
+          <Route path = '/login' component = { Login } />
+          <Route path = '/signup' component = { Signup } />
+          <Route path = '/user' component = { User } />
+        </Switch>
+      </div>
     </Router>
     );
 };
