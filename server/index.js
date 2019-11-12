293ccbe82bdb9814538cc313de92692f63d4700f
wendy-wm-wu <wendy.wm.wu@gmail.com>
2019-11-11 22:33:55 -0800

    Add bycrypt package and remove dependencies from package.json

--- server/index.js
+++ server/index.js
@@ -1,19 +1,21 @@
 const path = require('path');
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const axios = require('axios');
 const config = require('./api.config.js');
-
 const db = require('./controllers/index.js');
+const passport = require('passport');
+const bcrypt = require('bcrypt');
+
+const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use(express.static(__dirname + '/../../client/public'));
+app.use(express.static(path.join(__dirname, '/../../client/public')));
 app.use(express.static('./client/public'));
+
 
 app.post('/api/books', (req, res) => {
   let title = req.body.title;
@@ -97,5 +99,5 @@ app.get('/bestsellers', (req, res) => {
 });
 
 app.listen(3000, () => {
-  console.log('listening on port 3000!');
+  console.log('Book Connection is listening on port 3000!');
 });
